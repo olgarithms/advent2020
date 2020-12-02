0fdcae6905280cd2c24fb4ba4ece17f33469d657
Olga Matoula <omatoula@bloomberg.net>
2020-12-02 23:49:47 +0000

    Two

---
+++ two.py
@@ -0,0 +1,36 @@
+#!/usr/local/bin/python3.9
+
+from collections import Counter
+
+
+def a():
+    count = 0
+    with open("input.txt", "r") as f:
+        for line in f:
+            (policy, char, password) = line.split()
+            (low, high) = list(map(int, policy.split("-")))
+            char = char[0]
+            frequency = Counter(password)[char]
+            if frequency >= low and frequency <= high:
+                count += 1
+    return count
+
+
+def b():
+    count = 0
+    with open("input.txt", "r") as f:
+        for line in f:
+            (pos, char, password) = line.split()
+            (yes, no) = list(map(int, pos.split("-")))
+            char = char[0]
+            if (password[yes - 1] == char and password[no - 1] != char) or (
+                password[yes - 1] != char and password[no - 1] == char
+            ):
+                count += 1
+    return count
+
+
+if __name__ == "__main__":
+
+    print(a())
+    print(b())
